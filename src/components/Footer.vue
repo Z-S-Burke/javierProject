0788acb5afcd3440b0504bdaccfbf8203706827c
Z-S-Burke <zach@zsburke.com>
2020-04-20 10:33:51 -0500

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="d-flex align-items-center bg-dark text-light footer_style border border-top">
+      <router-link
+        to="/"
+        class="d-flex justify-content-center align-items-center footer_button bg-light text-dark"
+      >Home</router-link>
+      <router-link
+        to="/teams"
+        class="d-flex justify-content-center align-items-center footer_button bg-light text-dark"
+      >Bio</router-link>
+      <router-link
+        to="/players"
+        class="d-flex justify-content-center align-items-center footer_button bg-light text-dark"
+      >CV</router-link>
+      <router-link
+        to="/schedule"
+        class="d-flex justify-content-center align-items-center footer_button bg-light text-dark"
+      >Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.footer_style {
+  height: 50px;
+}
+.footer_button {
+  height: 100%;
+  width: 25%;
+  border-style: solid;
+  /* border-left-style: solid; */
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <b-navbar class="bg-dark" toggleable="lg" type="dark" variant="dark" top="fixed" id="navbar">
+      <b-navbar-brand>
+        <router-link to="/" class="d-flex align-items-center-text-light ml-2">
+          <h2 class="ml-2 mt-2 text-light">J. P. Sandoval</h2>
+        </router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="text-light" to="/">Home</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="text-light" to="/teams">Bio</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="text-light" to="/players">CV</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="text-light" to="/tournaments">Research</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="text-light" to="/schedule">Contact</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+
+<style scoped>
+.logo {
+  height: 50px;
+  width: 50px;
+}
+
+.logo_text {
+  text-decoration: none;
+}
+
+.custom_bg_color {
+  background-color: rgb(241, 144, 16);
+}
+</style> 
