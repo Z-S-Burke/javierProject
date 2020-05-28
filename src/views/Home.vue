e7aa318423176c9003d854f086340bfd494c3220
Z-S-Burke <zach@zsburke.com>
2020-05-28 10:47:57 -0500

    added content to home page and resized header photo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -54,13 +54,35 @@
         </router-link>
     </b-carousel>-->
 
-    <div class="m-3 bg-light text-dark carousel_border">
-      <h4>At a glance...</h4>
+    <div class="m-3 bg-light text-left text-dark carousel_border">
       <ul class="p-3">
-        <p>
-          Lorem ipsum dolor sit amet, aliquando inciderint per ea, vim ubique dissentiet ad. Vix eu commune invenire, per illud suavitate hendrerit cu, dolorum docendi eam cu. Aeque veritus ad has, sale feugait disputationi eam et, vis equidem commune cu. Facete graecis nam eu, id integre pericula iracundia sea. Nam ex quis aliquip saperet. Est enim audiam ut. Te ius alii deseruisse comprehensam.
-          Eius latine convenire vim te. Alii duis possit et mel. Lorem offendit mandamus at eum, in quem labore sea, vel eu etiam regione eruditi. Viris percipit et duo. Veniam ubique denique nam in, vel ei duis iudico. Nec id clita prompta sadipscing.
-        </p>
+        <p>I am a third year DPhil in Politics candidate based at Wolfson College. 
+          Before the DPhil, I completed a Master in Comparative Government also at Wolfson, 
+          and before coming to Oxford, I worked as a Junior Financial Manager at the Consejo 
+          Nacional de Ciencia y Tecnología. I earned my BA in Politics and Public Administration 
+          from Universidad Iberoamericana and I also earned a Research Methods Diploma from UNAM, 
+          both back in Mexico City.</p>
+          <p>
+            I am passionate about regime change, subnational politics, presidentialism, and the dynamics 
+            linking socio-economic development and politics. My research aims to increase our understanding 
+            of subnational regime variation across Latin America and beyond. 
+          </p>
+          <p>
+            Methods wise I’m an advocate for ‘theory-grounded eclecticism’. I strongly believe that quantitative 
+            literacy is fundamental and that at the core of every ‘useful model’ lies robust historical and case-specific 
+            knowledge. In learning and teaching CHA and ‘metrics, I encourage a hands-on and problem solving approach. 
+          </p>
+          <p>
+            At Oxford I work as a statistics GTA and I also teach the Latin American Politics tutorial to undergrads at the 
+            university. In the UK I’ve also worked as an Associate Lecturer at Brookes University for a similar course. 
+          </p>
+          <p>
+            I have a broad interest for the politics of the ‘developing world’ and a keen interest in 
+            Latin America, specifically in Argentinian, Brazilian and Mexican political dynamics. 
+          </p>
+          <p>
+            I enjoy working out, swimming and long walks. I’m also a Stacraft 2 fan, and a sci-fi, coffee and cinema aficionado.
+          </p>
       </ul>
     </div>
   </div>
@@ -107,6 +129,7 @@ export default {
 }
 
 .headerPhoto {
+  padding: 10%;
   max-width: 100%;
 }
 
